937bc738b8b586e0cefb4d0718f3fa747cd65f50
Anatoliy Evladov <visteras@yandex.ru>
2023-10-05 11:25:10 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+let todos = ref(['todo 1', 'todo 2', 'todo 3'])
+let newTodo = ref("")
+
+function addTodo() {
+  if (newTodo.value === "") return
+  todos.value.push(newTodo.value)
+}
+</script>
+
+<template>
+  <div>
+    <div>
+      <input v-model="newTodo"/>
+      <button @click="addTodo">add todo</button>
+    </div>
+    <div>
+      <p :key="todo" v-for="todo in todos">{{ todo }}</p>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
